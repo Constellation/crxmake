d177b7f79486bf8dc73d0e5bbcaa4c804243a942
Constellation <utatane.tea@gmail.com>
2011-07-20 14:20:44 +0900

    fix miscs

--- lib/crxmake.rb
+++ lib/crxmake.rb
@@ -9,7 +9,7 @@ require 'find'
 require 'pathname'
 
 class CrxMake < Object
-  VERSION = '2.0.4'
+  VERSION = '2.0.5'
   # thx masover
   MAGIC = 'Cr24'
 
@@ -159,7 +159,7 @@ ext dir: \"#{@exdir}\"
               Find.prune
             else
               puts "include dir: \"#{path}\"" if @verbose
-              zip.add(get_relative(@exdir, path))
+              zip.add(get_relative(@exdir, path), path)
             end
           else
             if @ignorefile && File.basename(path) =~ @ignorefile
